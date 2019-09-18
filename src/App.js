a8fe8394880730a2ee4cd655e92acaa84b135800
Eric Blancas <ericblancas23@gmail.com>
2019-09-17 21:36:59 -0700

    routing

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React from 'react';
 import './App.css';
 import { Switch ,Route } from 'react-router';
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats</h1>
-  </div>
-)
+import Shop from './pages/shop/shop.component';
+// const HatsPage = () => (
+//   <div>
+//     <h1>Hats</h1>
+//   </div>
+// )
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/hats' component={HatsPage} />
+        <Route exact path='/shop' component={Shop} />
       </Switch>
     </div>
   );
